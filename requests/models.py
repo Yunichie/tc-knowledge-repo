5a4a6abae8cc3e4a52140e09423404dd0ffc0a8d
nittsuj <justinvalentino88@gmail.com>
2026-04-27 00:27:50 +0700

    feat: create request, request details, list responses, fulfill payload

--- requests/models.py
+++ requests/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from resources.models import Resource
+from users.models import User
+
+
+class ResourceRequestStatus(models.TextChoices):
+    OPEN = "OPEN", _("Open")
+    FULFILLED = "FULFILLED", _("Fulfilled")
+
+
+class ResourceRequest(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.CharField(
+        max_length=20,
+        choices=ResourceRequestStatus.choices,
+        default=ResourceRequestStatus.OPEN,
+    )
+
+    requester = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="resource_requests"
+    )
+    fulfilled_by_resource = models.ForeignKey(
+        Resource,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="fulfills_request",
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "resource_requests"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
