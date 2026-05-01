d89f66cc52a76eb93b336050d401adb054c78988
Yunichie <alfarelsandriano@gmail.com>
2026-05-01 21:22:54 +0700

    feat: add allowed hosts and update workflow

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "True").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 # Apps
 INSTALLED_APPS = [
